f3ae64f0950efe82fb7a2313ee4d3bba46a2e051
zhangke <mpusher@zhangkedeMacBook-Pro.local>
2022-09-09 11:15:25 +0800

    update

--- src/Snowflake.php
+++ src/Snowflake.php
@@ -3,7 +3,7 @@ namespace Mpusher\Snowflake;
 
 use Godruoyi\Snowflake\Snowflake as Snow;
 use Godruoyi\Snowflake\RedisSequenceResolver;
-use think\facade\Cache;
+use support\Redis;
 
 /**
  * 雪花生成器
@@ -28,12 +28,7 @@ class Snowflake
     {
         static::$snowflake = new Snow($config['data_center_id'], $workerId);
         static::$snowflake->setStartTimeStamp($config['start_time'] ?? strtotime('Y-m-d') * 1000);
-        $redis = $config['redis'] ?? [];
-        if (empty($redis)) {
-            throw new \Exception('Snowflake: Redis Options Empty!!!');
-        }
-        $redis['type'] = 'redis';
-        $seq = new RedisSequenceResolver(Cache::connect($redis)->handler());
+        $seq = new RedisSequenceResolver(Redis::connection()->client());
         static::$snowflake->setSequenceResolver($seq);
     }
 
--- src/config/plugin/mpusher/snowflake/app.php
+++ src/config/plugin/mpusher/snowflake/app.php
@@ -4,11 +4,5 @@ return [
     'snowflake' => [
         'date_center_id' => '',
         'start_time' => strtotime('Y-m-d') * 1000,
-        'redis' => [
-            'host'   => '',
-            'port'   => 6379,
-            'password' => '',
-            'select' => 0,
-        ],
     ],
 ];
